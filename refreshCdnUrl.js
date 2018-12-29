15de42a53a30ac306fe7283c6783c6061870c8d6
Alex <observable@163.com>
2018-12-29 16:20:41 +0800

    发布到oss后刷新cdn

---
+++ refreshCdnUrl.js
@@ -0,0 +1,14 @@
+const qcloudSDK = require('qcloud-cdn-node-sdk');
+
+qcloudSDK.config({
+    secretId: 'AKIDpnwvriryPhpmY8ssAIF1c2Vs33wdCTbY',
+    secretKey: 'ufKo7EAvUr34wnYEkQ9pTxHn8JMFxUq1'
+})
+
+qcloudSDK.request('RefreshCdnUrl', {
+	'urls.0': 'https://www.bonjs.com'
+}, (res) => {
+    if(JSON.parse(res).code == 0) {
+		console.log('成功刷新cdn')	
+    }
+})
